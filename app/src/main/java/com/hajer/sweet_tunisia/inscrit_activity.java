3e626a62be571a2a07760b83a592c7190c2ff029
TEST <test@test.com>
2019-12-04 11:12:51 +0100

    ajout de l'activitée authentification

---
+++ app/src/main/java/com/hajer/sweet_tunisia/inscrit_activity.java
@@ -0,0 +1,13 @@
+package com.hajer.sweet_tunisia;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class inscrit_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscrit);
+    }
+}
---
+++ app/src/main/java/com/hajer/sweet_tunisia/login_activity_display.java
@@ -0,0 +1,28 @@
+package com.hajer.sweet_tunisia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login_activity_display extends AppCompatActivity {
+
+    final String EXTRA_LOGIN = "email";
+    final String EXTRA_PASSWORD = "password";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.authentification);
+
+        Intent intent = getIntent();
+        TextView loginDisplay = findViewById(R.id.email);
+        TextView passwordDisplay = findViewById(R.id.password);
+
+        if (intent != null) {
+            loginDisplay.setText(intent.getStringExtra(EXTRA_LOGIN));
+            passwordDisplay.setText(intent.getStringExtra(EXTRA_PASSWORD));
+        }
+    }
+}
