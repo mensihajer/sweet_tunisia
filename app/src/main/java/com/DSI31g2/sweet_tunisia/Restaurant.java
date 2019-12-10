da8e86cdf799c882b0d65fb404fbc7ccdf9f9f2f
saharsaafi <saharsaafi123@gmail.com>
2019-12-11 00:02:16 +0100

    sqllite

---
+++ app/src/main/java/com/DSI31g2/sweet_tunisia/Restaurant.java
@@ -0,0 +1,27 @@
+package com.DSI31g2.sweet_tunisia;
+
+public class Restaurant {
+    private String describe;
+    private String localisation;
+
+    public Restaurant(String describe, String localisation) {
+        this.describe = describe;
+        this.localisation = localisation;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public String getLocalisation() {
+        return localisation;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public void setLocalisation(String localisation) {
+        this.localisation = localisation;
+    }
+}
